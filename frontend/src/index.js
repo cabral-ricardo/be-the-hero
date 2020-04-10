fc38cc26acd3f139b422e7906d4d4d35c56375ff
ricardo <ricardocabral.net@gmail.com>
2020-04-10 17:16:48 -0300

    include sentry front

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_DNS_SENTRY,
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
